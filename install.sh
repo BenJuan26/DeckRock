d84de3dd1b3adbb869a3387e964eb0c4c0e35320
Benjamin Schubert <brm.schubert@gmail.com>
2026-04-15 14:42:22 -0400

    Theoretically finished. Completely untested.

--- install.sh
+++ install.sh
@@ -1,75 +1,149 @@
 #!/bin/bash
 SCRIPT_DIR=$PWD
 
-SEARCH_RESULTS=$(unzip -Z1 MCWindows.zip | grep Minecraft.Windows.exe)
-if [ "$SEARCH_RESULTS" != "Minecraft.Windows.exe" ]; then
-    echo "Invalid game contents: couldn't find Minecraft.Windows.exe at the top level" && exit 1
-fi
-
 MC_DIR=$HOME/Minecraft
-MC_CONTENTS=$MC_DIR/Contents
-mkdir -p $MC_CONTENTS
-unzip MCWindows.zip -d $MC_CONTENTS
 
-# this might not work because of cloudflare
-wget https://mirror.msys2.org/mingw/mingw64/mingw-w64-x86_64-curl-8.17.0-1-any.pkg.tar.zst
-tar -xf mingw-w64-x86_64-curl-8.17.0-1-any.pkg.tar.zst mingw64/bin/libcurl-4.dll
-# Overwrite built-in XCurl.dll
-mv libcurl-4.dll $MC_CONTENTS/XCurl.dll
+install_minecraft() {
+    echo -n "Extracting Minecraft... "
+    SEARCH_RESULTS=$(unzip -Z1 MCWindows.zip | grep Minecraft.Windows.exe)
+    if [ "$SEARCH_RESULTS" != "Minecraft.Windows.exe" ]; then
+        echo "Invalid game contents: couldn't find Minecraft.Windows.exe at the top level" && exit 1
+    fi
 
-CERTS_DIR=$MC_DIR/etc/ssl/certs
-mkdir -p $CERTS_DIR
-wget https://curl.se/ca/cacert.pem
-mv cacert.pem $CERTS_DIR/ca-bundle.crt
+    MC_CONTENTS=$MC_DIR/Contents
+    mkdir -p $MC_CONTENTS
+    unzip MCWindows.zip -d $MC_CONTENTS
+    echo "done."
+}
 
-PROTON_RELEASE_INFO=$(curl -s https://api.github.com/repos/Weather-OS/GDK-Proton/releases/latest)
-PROTON_RELEASE_URL=$(echo "$PROTON_RELEASE_INFO" | jq -r .assets[0].browser_download_url)
-PROTON_RELEASE_FILENAME=$(echo "$PROTON_RELEASE_INFO" | jq -r .assets[0].name)
-wget $PROTON_RELEASE_URL
-tar -zxf $PROTON_RELEASE_FILENAME -C $HOME/.steam/root/compatibilitytools.d
+patch_curl() {
+    echo -n "Patching networking tools... "
+    # this might not work because of cloudflare
+    wget https://mirror.msys2.org/mingw/mingw64/mingw-w64-x86_64-curl-8.17.0-1-any.pkg.tar.zst || exit 1
+    tar -xf mingw-w64-x86_64-curl-8.17.0-1-any.pkg.tar.zst mingw64/bin/libcurl-4.dll
+    # Overwrite built-in XCurl.dll
+    mv libcurl-4.dll $MC_CONTENTS/XCurl.dll
 
-# Need to restart Steam
+    CERTS_DIR=$MC_DIR/etc/ssl/certs
+    mkdir -p $CERTS_DIR
+    wget https://curl.se/ca/cacert.pem || exit 1
+    mv cacert.pem $CERTS_DIR/ca-bundle.crt
+    echo "done."
+}
 
-JAVA_RELEASE_FULL_JSON=$(curl -s https://api.github.com/repos/adoptium/temurin25-binaries/releases/latest)
-JAVA_RELEASE_JSON=$(echo $JAVA_RELEASE_FULL_JSON | jq '.assets[] | select (.name | test("jdk_x64_linux_hotspot.*tar.gz$"))')
-JAVA_RELEASE_URL=$(echo $JAVA_RELEASE_JSON | jq -r .browser_download_url)
-JAVA_RELEASE_FILENAME=$(echo $JAVA_RELEASE_JSON | jq -r .name)
-wget $JAVA_RELEASE_URL
-JDK_DIR_NAME=$(tar -ztf $JAVA_RELEASE_FILENAME | head -1)
-tar -zxf $JAVA_RELEASE_FILENAME -C $HOME/.local
-JAVA_HOME=$HOME/.local/$JDK_DIR_NAME
-echo "JAVA_HOME=$HOME/.local/$JDK_DIR_NAME" >> $HOME/.bash_profile
-echo 'PATH=$PATH:$JAVA_HOME/bin' >> $HOME/.bash_profile
-JAVA_EXE=$JAVA_HOME/bin/java
+install_gdk_proton() {
+    echo -n "Installing GDK-Proton... "
+    EXISTING_GE_COUNT=$(ls $HOME/.steam/root/compatibilitytools.d | grep -e '^GE' -e '^GDK' | wc -l)
+    if [ "${EXISTING_GE_COUNT:-0}" -gt 0 ]; then
+        echo "already installed." && return
+    fi
+    PROTON_RELEASE_INFO=$(curl -s https://api.github.com/repos/Weather-OS/GDK-Proton/releases/latest)
+    PROTON_RELEASE_URL=$(echo "$PROTON_RELEASE_INFO" | jq -r .assets[0].browser_download_url)
+    PROTON_RELEASE_FILENAME=$(echo "$PROTON_RELEASE_INFO" | jq -r .assets[0].name)
+    wget $PROTON_RELEASE_URL || exit 1
+    tar -zxf $PROTON_RELEASE_FILENAME -C $HOME/.steam/root/compatibilitytools.d
+    echo "done."
+}
 
-PROXY_PASS_DIR=$HOME/ProxyPass
-wget https://github.com/Kas-tle/ProxyPass/releases/latest/ProxyPass.jar
-mkdir -p $PROXY_PASS_DIR
-mv ProxyPass.jar $PROXY_PASS_DIR/
-mv wrapper.sh $PROXY_PASS_DIR/
+install_java() {
+    echo -n "Installing java... "
+    if command -v java > /dev/null; then
+        JAVA_EXE=java
+        echo "already installed." && return
+    fi
+    JAVA_RELEASE_FULL_JSON=$(curl -s https://api.github.com/repos/adoptium/temurin25-binaries/releases/latest)
+    JAVA_RELEASE_JSON=$(echo $JAVA_RELEASE_FULL_JSON | jq '.assets[] | select (.name | test("jdk_x64_linux_hotspot.*tar.gz$"))')
+    JAVA_RELEASE_URL=$(echo $JAVA_RELEASE_JSON | jq -r .browser_download_url)
+    JAVA_RELEASE_FILENAME=$(echo $JAVA_RELEASE_JSON | jq -r .name)
+    wget $JAVA_RELEASE_URL || exit 1
+    JDK_DIR_NAME=$(tar -ztf $JAVA_RELEASE_FILENAME | head -1)
+    tar -zxf $JAVA_RELEASE_FILENAME -C $HOME/.local
+    export JAVA_HOME=$HOME/.local/$JDK_DIR_NAME
+    echo "JAVA_HOME=$HOME/.local/$JDK_DIR_NAME" >> $HOME/.bash_profile
+    echo 'PATH=$PATH:$JAVA_HOME/bin' >> $HOME/.bash_profile
+    JAVA_EXE=$JAVA_HOME/bin/java
+    echo "done."
+}
 
-# Run ProxyPass once to generate config (and potentially log in?)
-cd $PROXY_PASS_DIR
-$JAVA_EXE -jar ProxyPass.jar > output.log 2>&1 &
-PP_PID=$!
-echo "Waiting for link code..."
-MS_CODE_LINE=""
-while [ -z "$MS_CODE_LINE" ]; do
-    MS_CODE_LINE=$(cat output.log | grep "TODO-FIND-SEARCH-STRING")
-    sleep 1
-done
+install_proxy_pass() {
+    echo -n "Installing ProxyPass... "
+    PROXY_PASS_DIR=$HOME/ProxyPass
+    wget https://github.com/Kas-tle/ProxyPass/releases/latest/ProxyPass.jar
+    mkdir -p $PROXY_PASS_DIR
+    mv ProxyPass.jar $PROXY_PASS_DIR/
+    mv wrapper.sh $PROXY_PASS_DIR/
+    echo "done."
+}
 
-echo $MS_CODE_LINE
-echo "Waiting for link to complete..."
-while [ ! -f auth.json ]; do
-    sleep 1
-done
+sign_in() {
+    # Run ProxyPass once to generate config and potentially log in
+    cd $PROXY_PASS_DIR
+    $JAVA_EXE -jar ProxyPass.jar > output.log 2>&1 &
+    PP_PID=$!
+    echo "Waiting for link code..."
+    MS_CODE=""
+    while [ -z "$MS_CODE" ]; do
+        MS_CODE=$(cat output.log | sed -rn 's/.*Enter code ([A-Z0-9]{8})$/\1/p')
+        sleep 1
+    done
 
-kill -1 $PP_PID
+    # Copy code to clipboard
+    echo "$MS_CODE" | xargs qdbus org.kde.klipper /klipper org.kde.klipper.klipper.setClipboardContents
 
-# Apply the host to the proxy config
-PROXY_PASS_DESTINATION_HOST=192.168.1.100 # example
-sed -i '/destination/,${/host\: .*/{s/host: .*/host\: '"${PROXY_PASS_DESTINATION_HOST}"'/; :a; n; ba}}' config.yml
+    echo "Code is $MS_CODE"
+    echo "It has been copied to your clipboard."
+    echo "Now opening the browser to sign in. Paste the code that has been copied."
+    echo "If the browser does not open, navigate to https://www.microsoft.com/link"
+    echo -n "Waiting for link to complete... "
 
-# Restart steam, tick the box for "Force the use of a specific Steam Play compatibility tool"
-# Set the launch options to: $HOME/ProxyPass/wrapper.sh %command%
+    xdg-open https://www.microsoft.com/link
+    while [ ! -f auth.json ]; do
+        sleep 1
+    done
+    echo "done."
+
+    kill -1 $PP_PID
+}
+
+configure_proxy_pass() {
+    # Apply the host to the proxy config
+    read -p "Enter the hostname or IP address of the destination server:" PROXY_PASS_DESTINATION_HOST
+    if [ -z "$PROXY_PASS_DESTINATION_HOST" ]; then
+        echo "Must provide a non-empty host for the destination server" && exit 1
+    fi
+    sed -i '/destination/,${/host\: .*/{s/host: .*/host\: '"${PROXY_PASS_DESTINATION_HOST}"'/; :a; n; ba}}' config.yml
+    echo "Configuration updated."
+}
+
+post_install() {
+    MSG=$(cat <<EOF
+Installation complete!
+
+The next steps MUST be done manually:
+1. Fully restart steam.
+2. Navigate to $MC_CONTENT
+3. Right-click Minecraft.Windows.exe and select Add to Steam.
+4. Go to the Steam library, right-click the newly added shortcut, and click Properties.
+5. Enter this text in the Launch Options box:
+$PROXY_PASS_DIR/wrapper.sh %command%
+6. Under Compatibility, choose the added GE-Proton version (e.g. GE-Proton10-32).
+
+Once that is complete, you're finished! The game can be launched normally through Steam.
+ProxyPass will start up before Minecraft launches and shut down after it exits.
+To change which server it is pointing to, change the 'host' value under 'destination' in the config file, which is located here:
+$PROXY_PASS_DIR/config.yml
+
+Punch wood, get good!
+EOF
+    )
+    echo "$MSG"
+}
+
+install_minecraft
+patch_curl
+install_gdk_proton
+install_java
+install_proxy_pass
+sign_in
+configure_proxy_pass
+post_install
